c1e7eb4cb69422cd8337885148cc1d96254e2350
yuxiao11111 <977736102@qq.com>
2021-05-17 10:10:42 +0800

    no message

--- assets/scripts/scene/Main.ts
+++ assets/scripts/scene/Main.ts
@@ -10,10 +10,12 @@ export default class Main extends GFBindData {
 
     // onLoad () {}
 
-    
+    //折纸片（纸片折好，然后画图，然后展开，将图放在旁边，让玩家根据画好的图折出来）
+    //按顺序亮灯，然后让玩家重复
+    //
 
     start () {
-
+        
     }
 
     // update() {
